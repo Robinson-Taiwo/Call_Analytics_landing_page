06ea978e4fcae05a00cd09846471f937c50f9137
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-08-30 16:13:29 +0100

    WIP: implemented hero section background

---
+++ src/app/components/data.ts
@@ -0,0 +1,16 @@
+const data: { title: string; description: string }[] = [
+    {
+      title: "Visual Clarity",
+      description: "Ensure that data is presented clearly and concisely. Use charts, graphs, and other visual aids to make complex call analytics information easy to understand at a glance.",
+    },
+    {
+      title: "Accessibility",
+      description: "Design the UI to be accessible to users with disabilities. This includes considerations such as color contrast, keyboard navigation, and screen reader compatibility.",
+    },
+    {
+      title: "User-Friendly Interface",
+      description: "The UI should be intuitive and easy to navigate, even for users who may not be financial experts. Consider employing familiar design patterns and clear labeling.",
+    },
+  ];
+
+  export default data
