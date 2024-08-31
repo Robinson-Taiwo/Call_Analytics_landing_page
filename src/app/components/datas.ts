7f04225f4ffc5cbdac850579580e1a8469ad5818
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-08-31 23:22:35 +0100

    WIP: worked on design section

---
+++ src/app/components/datas.ts
@@ -0,0 +1,47 @@
+export const telecomAnalysisData:  { title: string; description: string }[] = [
+    {
+      title: "Company Objective",
+      description: "A telecommunications company wants to analyze their call data to improve customer service and optimize operational efficiency."
+    },
+    {
+      title: "Call Records",
+      description: "The company has millions of call records containing information such as call duration, time of call, caller ID, call type (e.g., sales inquiry, technical support, billing), and outcome (e.g., issue resolved, escalation needed)."
+    },
+    {
+      title: "Data Analysis Goal",
+      description: "The company aims to extract actionable insights from this data, identifying patterns in call volume and duration throughout the day, week, and month."
+    },
+    {
+      title: "Challenges",
+      description: "Insufficient training programs can result in agents lacking the necessary skills and knowledge to handle customer inquiries effectively, leading to lower customer satisfaction and higher call resolution times."
+    },
+    {
+      title: "Optimization and Prediction",
+      description: "The company seeks to determine peak call times to optimize staffing levels, analyze call types and outcomes to identify common issues, and predict future call volumes to better allocate resources."
+    }
+  ];
+  
+
+  export const telecomTasks:  { title: string; description: string }[] = [
+    {
+      title: "Data Cleaning",
+      description: "Clean the data to remove duplicates, inconsistencies, and missing values."
+    },
+    {
+      title: "Time Series Analysis",
+      description: "Use time series analysis techniques such as moving averages or Fourier transforms to identify recurring patterns in call volume. Determine peak call hours, days, and months to adjust staffing levels accordingly."
+    },
+    {
+      title: "Data Visualization",
+      description: "Visualize call volume trends over time using line plots or histograms. Analyze call duration distributions to identify outliers and potential issues."
+    },
+    {
+      title: "Call Type Analysis",
+      description: "Group calls by type and analyze the distribution of outcomes for each type. Use techniques like sentiment analysis or topic modeling to identify common issues discussed during calls."
+    },
+    {
+      title: "Dashboard Implementation",
+      description: "Implement a dashboard or reporting system to monitor call metrics in real-time. Regularly review performance metrics and update models as needed to adapt to changing conditions and customer needs. Solicit feedback from customer service representatives and customers to identify areas for improvement."
+    }
+  ];
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import { PenTool } from "lucide-react";
+import { Compass, FileText, Lightbulb, PenTool, TestTube } from "lucide-react";
 import Image from "next/image";
 import HeroImage from "/images/bg.png";
 import data from "./components/data";
+import { telecomAnalysisData, telecomTasks } from "./components/datas";
+import { File } from "lucide";
 
 export default function Home() {
   return (
@@ -15,7 +17,7 @@ export default function Home() {
         <div className="about-this-section bg-[#f6f7fa] pt-[3.063rem] px-[2rem]  mt-[3.25rem] ">
           <h1 className="text-[3rem]  font-bold  ">About The Project</h1>
 
-          <p className="mt-[1.75rem] w-[80%]  mb-[4.19rem] text-[#696b6b]  ">
+          <p className="mt-[1.75rem] w-[80%] text-[1.25rem]  mb-[4.19rem] text-[#696b6b]  ">
             Call Analytics is an innovative cloud-based software designed to
             meet the dynamic needs of people Through this, all the problems
             related to call operators of people&#x27;s mobile phones are solved.
@@ -38,7 +40,150 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="third"></section>
+      <section className="bg-white px-[2rem]  flex flex-col ">
+        <div className="third pt-[8rem] mb-[8rem]   flex flex-row  w-[100%]  justify-between  text-[3rem] text-center ">
+          <div className="flex flex-col  text-[3rem] w-[100%] ">
+            <span className=" text-[3.5rem] font-bold ">14</span>
+            <span className=" text-[2rem] font-semibold ">Weeks</span>
+          </div>
+
+          <div
+            className="flex flex-col w-[100%] border-l-[3.5px] border-[black] text-[3rem] "
+            style={{
+              borderImage:
+                "linear-gradient(to bottom, transparent, black, transparent) 1 100%",
+            }}
+          >
+            <span className=" text-[3.5rem] font-bold ">10+</span>
+            <span className=" text-[2rem] font-semibold ">Survey</span>
+          </div>
+
+          <div
+            className="flex flex-col w-[100%] border-l-[3.5px] border-[black] text-[3rem] "
+            style={{
+              borderImage:
+                "linear-gradient(to bottom, transparent, black, transparent) 1 100%",
+            }}
+          >
+            <span className=" text-[3.5rem] font-bold ">10+</span>
+            <span className=" text-[2rem] font-semibold ">Web Screen</span>
+          </div>
+
+          <div
+            className="flex flex-col w-[100%] border-l-[3px] border-[black] text-[3rem] "
+            style={{
+              borderImage:
+                "linear-gradient(to bottom, transparent, black, transparent) 1 100%",
+            }}
+          >
+            <span className=" text-[3.5rem] font-bold ">10+</span>
+            <span className=" text-[2rem] font-semibold ">App Screen</span>
+          </div>
+        </div>
+        <div className=" rounded-[0.625rem] flex flex-col mb-[3rem] p-[2rem] bg-[#f6f7fa]  ">
+          <div className="border-[red] font-bold text-[red] w-fit border-[2px] py-[0.5rem] px-[0.5rem] rounded-3xl ">
+            Problem Statement
+          </div>
+
+          <h1 className="  mt-[2rem] text-[2.2rem] font-semibold mb-[2rem] ">
+            We surveyed users to a find out what problems they face when working
+            with such applications
+          </h1>
+
+          <div className=" border-dashed border-[red] p-[1rem] border-[2px] rounded-[0.625rem]  ">
+            {telecomAnalysisData.map((item, index) => (
+              <ul
+                key={index}
+                className="flex list-disc flex-col gap-[1rem] text-[#696b6b] text-[1.25rem] p-[1rem] "
+              >
+                <li>{item.description}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+
+        <div className=" rounded-[0.625rem] flex flex-col p-[2rem] bg-[#f6f7fa]  ">
+          <div className="border-[#50cc16] font-bold text-[#50cc16] w-fit border-[2px] py-[0.5rem] px-[0.5rem] rounded-3xl ">
+            Possible Solution
+          </div>
+
+          <h1 className="  mt-[2rem] text-[2.2rem] font-semibold mb-[2rem] ">
+            We have surveyed the users and analyzed their problems, now their
+            problems have been solved.
+          </h1>
+
+          <div className=" border-dashed border-[#50cc16] border-[2px] rounded-[0.625rem] p-[1rem] ">
+            {telecomTasks.map((item, index) => (
+              <ul
+                key={index}
+                className="flex flex-col gap-[1rem] list-disc text-[#696b6b] text-[1.25rem] p-[1rem] "
+              >
+                <li>{item.description}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className=" mt-[3rem] px-[2rem]  ">
+        <div className="design-proccess flex items-center justify-center flex-col  bg-[#f6f7fa] gap-[2rem] py-[2rem] ">
+          <h1 className=" font-bold text-[3rem] ">Design Thinking Process</h1>
+
+          <p className=" w-[60%] text-center text-[1.25rem] text-[#696b6b] ">
+            Design thinking is a problem-solving methodology that focuses on
+            understanding the needs and preferences of end-users to develop
+            innovative solutions. It&#x27;s a human-centered approach that
+            emphasizes empathy, creativity, and iteration. Here&#x27;s a brief
+            overview of the design thinking process:
+          </p>
+        </div>
+        <div className="flex items-center justify-center ">
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem] bg-[#e5f8dd] rounded-[50%] "></div>
+            <div className=" border-[1px] border-[black] border-dashed w-[3rem] h-[0.01rem]  "></div>
+          </div>
+
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem]  bg-[#e5f8dd] rounded-[50%] items-center flex justify-center  ">
+              <Compass className=" text-[#50cc16] h-[3.06rem] w-[3.06rem] " />
+            </div>
+            <div className=" border-[1px] border-[black] border-dashed w-[3rem] h-[0.01rem]  "></div>
+          </div>
+
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem] bg-[#e5f8dd] rounded-[50%] items-center flex justify-center ">
+              <FileText className=" text-[#50cc16] h-[3.06rem] w-[3.06rem] " />
+            </div>
+            <div className=" border-[1px] border-[black] border-dashed w-[3rem] h-[0.01rem]  "></div>
+          </div>
+
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem] bg-[#e5f8dd] rounded-[50%] items-center flex justify-center ">
+              {" "}
+              <Lightbulb className=" text-[#50cc16] h-[3.06rem] w-[3.06rem] " />{" "}
+            </div>
+            <div className=" border-[1px] border-[black] border-dashed w-[3rem] h-[0.01rem]  "></div>
+          </div>
+
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem] bg-[#e5f8dd] rounded-[50%] items-center flex justify-center ">
+              <PenTool className=" rotate-[270deg] text-[#50cc16] h-[3.06rem] w-[3.06rem] " />
+            </div>
+            <div className=" border-[1px] border-[black] border-dashed w-[3rem] h-[0.01rem]  "></div>
+          </div>
+
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem] bg-[#e5f8dd] rounded-[50%] items-center flex justify-center ">
+              <TestTube className=" text-[#50cc16] h-[3.06rem] w-[3.06rem] " />
+            </div>
+            <div className=" border-[1px] border-[black] border-dashed w-[3rem] h-[0.01rem]  "></div>
+          </div>
+
+          <div className=" flex flex-row  items-center justify-center ">
+            <div className=" h-[7.25rem] w-[7.25rem] bg-[#e5f8dd] rounded-[50%] "></div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
