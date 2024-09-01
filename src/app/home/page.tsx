38def8d7ef73407ddab63622fbd08fb1f57ee45f
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-09-01 21:11:38 +0100

    WIP: implementing user details on dashboard page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
-import { Ellipsis } from "lucide-react";
+import { ArrowUpRight, Ellipsis } from "lucide-react";
 import React from "react";
 import { Component } from "./components/Charts";
+import Image from "next/image";
 
 const page = () => {
   return (
@@ -60,16 +61,117 @@ const page = () => {
         </div>
       </div>
 
-      <div className="w-[100%] mt-[2rem] gap-[2rem]  ">
-        <div className=" w-[50%] ">
+      <div className="w-[100%] mt-[2rem] flex-row flex gap-[2rem]  ">
+        <div className=" w-[50%] rounded-[1rem] ">
           <Component />
         </div>
 
-        <div className=" w-[50%]  p-[1rem]  ">
+        <div className=" w-[50%] flex flex-col border  rounded-[1rem] p-[2rem]  ">
+          <div className="flex flex-row justify-between mb-[1rem] ">
+            <h1>Users Details </h1>
 
+            <h2>weekly</h2>
+          </div>
 
+          <div className="grid grid-cols-2 gap-[1rem] h-full sm:grid-cols-2">
+            {/* First Card */}
+            <div className="flex rounded-[1.2rem] p-[1rem] flex-row bg-gray-100 h-[80%] ">
+              <div className="w-[50%] relative rounded-[2rem]">
+                <Image
+                  src="/images/avatar.jpg"
+                  alt="Logo"
+                  layout="fill"
+                  className="rounded-[2rem]"
+                  objectFit="cover" // or 'contain', depending on your preference
+                />
+              </div>
+              <div className="flex flex-col w-[50%] justify-between">
+                <div>
+                  <h1 className="font-bold text-[1.225rem]">Satish Kumar</h1>
+                  <h3 className="font-bold text-[grey] text-[1rem]">Manager</h3>
+                </div>
+                <div>
+                  <p>view profile</p>
+                </div>
+              </div>
+            </div>
 
+            {/* Second Card */}
+            <div className="flex rounded-[1.2rem] p-[1rem] flex-row bg-gray-100 h-[80%] ">
+              <div className="w-[50%] relative rounded-[2rem]">
+                <Image
+                  src="/images/avatar.jpg"
+                  alt="Logo"
+                  layout="fill"
+                  className="rounded-[2rem]"
+                  objectFit="cover" // or 'contain', depending on your preference
+                />
+              </div>
+              <div className="flex flex-col w-[50%] justify-between">
+                <div>
+                  <h1 className="font-bold text-[1.225rem]">Satish</h1>
+                  <h3 className="font-bold text-[grey] text-[1rem]">Manager</h3>
+                </div>
+                <div>
+                  <p>view profile</p>
+                </div>
+              </div>
+            </div>
 
+            {/* Third Card */}
+            <div className="flex rounded-[1.2rem] p-[1rem] flex-row  h-[80%] ">
+              <div className="w-[50%] relative rounded-[2rem]">
+                <Image
+                  src="/images/avatar.jpg"
+                  alt="Logo"
+                  layout="fill"
+                  className="rounded-[2rem]"
+                  objectFit="cover" // or 'contain', depending on your preference
+                />
+              </div>
+              <div className="flex flex-col w-[50%] justify-between">
+                <div className="flex flex-col gap-[1rem] ">
+                  <h1 className="font-bold text-[1.225rem] text-center ">
+                    Satish Kumar
+                  </h1>
+                  <h3 className="font-bold text-center text-[#6c6c6c] text-[1rem]">
+                    Manager
+                  </h3>
+                </div>
+                <div>
+                  <div className="flex flex-row mx-[1rem] bg-[#eeeeee] rounded-[1.5rem] items-center justify-between font-semibold  p-[0.05rem] ">
+                    <p>view profile</p>
+
+                    <p className=" p-[0.5rem] rounded-[50%] bg-[green]  ">
+                      <ArrowUpRight className="text-white" />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Fourth Card */}
+            <div className="flex rounded-[1.2rem] p-[1rem] flex-row bg-gray-100 h-[80%] ">
+              <div className="w-[50%] relative rounded-[2rem]">
+                <Image
+                  src="/images/avatar.jpg"
+                  alt="Logo"
+                  layout="fill"
+                  className="rounded-[2rem]"
+                  objectFit="cover" // or 'contain', depending on your preference
+                />
+              </div>
+              <div className="flex flex-col w-[50%] justify-between">
+                <div>
+                  <h1 className="font-bold text-[1.225rem]">Satish</h1>
+                  <h3 className="font-bold text-[grey] text-[1rem]">Manager</h3>
+                </div>
+                <div>
+                  <p>view profile</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
